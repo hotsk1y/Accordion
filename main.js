cb139938d052e310ee9da0f5e9e13178f270a917
Aleksandr Hotskiy <hotskyi.aleksandr@gmail.com>
2021-03-29 13:21:50 +0300

    done

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 const title = document.querySelectorAll('.title')
 const btn = document.querySelector('#btn')
 
+function addFirstItems(event) {
+  const descrItem = event.target.parentElement
+  descrItem.classList.toggle('active')
+}
+
 const firstInterface = () => {
   for (let i = 0; i < title.length; i++) {
-    title[i].addEventListener('click', (event) => {
-      const descrItem = event.target.parentElement
-      descrItem.classList.toggle('active')
-    })
+    title[i].removeEventListener('click', addSecondItems)
   }
+
+  for (let i = 0; i < title.length; i++) {
+    title[i].addEventListener('click', addFirstItems)
+  }
+}
+
+function addSecondItems(event) {
+  const descrItem = event.target.parentElement
+  descrItem.classList.toggle('active')
+  for (let j = 0; j < title.length; j++) {
+    if (title[j].parentElement.classList.contains('active')) {
+      title[j].parentElement.classList.remove('active')
+    }
+  }
+  descrItem.classList.toggle('active')
 }
 
 const secondInterface = () => {
   for (let i = 0; i < title.length; i++) {
-    title[i].addEventListener('click', (event) => {
-      const descrItem = event.target.parentElement
-      descrItem.classList.toggle('active')
-      for (let j = 0; j < title.length; j++) {
-        if (title[j].parentElement.classList.contains('active')) {
-          title[j].parentElement.classList.remove('active')
-        }
-      }
-      descrItem.classList.toggle('active')
-    })
+    title[i].removeEventListener('click', addFirstItems)
+  }
+
+  for (let i = 0; i < title.length; i++) {
+    title[i].addEventListener('click', addSecondItems)
   }
 }
 
@@ -34,7 +46,7 @@ const startInterface = () => {
 }
 
 const changeInterface = () => {
-  btn.addEventListener('click', () => {    
+  btn.addEventListener('click', () => {
     if (btn.value === 'second') {
       btn.value = 'first'
     } else {
